126b5e3dc7c9c8bce0ae582623f85718f442bacb
Safal Bhatta <safalbhatta1@gmail.com>
2026-06-19 10:56:07 +0545

    comits

--- src/main/java/ASLENIX/pharmacy/demo/repository/ScheduleTrackerRepository.java
+++ src/main/java/ASLENIX/pharmacy/demo/repository/ScheduleTrackerRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ScheduleTrackerRepository extends JpaRepository<ScheduleTracker , UUID> {
-    ScheduleTracker findByJobType(JobType jobTypes);
+    ScheduleTracker findByJobType(JobType jobType);
 
 
 }
