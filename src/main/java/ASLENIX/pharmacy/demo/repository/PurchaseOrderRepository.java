fc26fca78526d32afedba096b3f520b36c907de0
aayush rana magar <realnaramagaraayush@gmail.com>
2026-07-01 11:03:43 +0545

    added sum query

--- src/main/java/ASLENIX/pharmacy/demo/repository/PurchaseOrderRepository.java
+++ src/main/java/ASLENIX/pharmacy/demo/repository/PurchaseOrderRepository.java
@@ -3,11 +3,17 @@ package ASLENIX.pharmacy.demo.repository;
 import ASLENIX.pharmacy.demo.Enums.PaymentStatus;
 import ASLENIX.pharmacy.demo.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder , Long> {
 
     List<PurchaseOrder> findByPaymentStatus(PaymentStatus paymentStatus);
+
+    @Query("SELECT SUM(p.grandTotal) FROM PurchaseOrder p WHERE p.receivedDate BETWEEN :start AND :end")
+    Double sumPurchasesBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
 
 }
