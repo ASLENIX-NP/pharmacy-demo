9ce3c4532bd58f717741f97ad0d1ee88987cf1d5
aayush rana magar <realnaramagaraayush@gmail.com>
2026-07-13 10:56:22 +0545

    added and implemented total unit sold this month

--- src/main/java/ASLENIX/pharmacy/demo/repository/FinanceStatsRepository.java
+++ src/main/java/ASLENIX/pharmacy/demo/repository/FinanceStatsRepository.java
@@ -2,7 +2,10 @@ package ASLENIX.pharmacy.demo.repository;
 
 import ASLENIX.pharmacy.demo.model.FinanceStats;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Locale;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +13,25 @@ public interface FinanceStatsRepository extends JpaRepository<FinanceStats , UUI
 
     Optional<FinanceStats> findByYearAndMonth(int year, int month);
 
+    // This fetches ONLY the totalRevenue field for a specific year and month
+    @Query("SELECT f.totalRevenue FROM FinanceStats f WHERE f.year = :year AND f.month = :month")
+    Optional<Double> findTotalRevenueByYearAndMonth(
+            @Param("year") int year,
+            @Param("month") int month
+    );
+
+
+    // This fetches ONLY the purchaseOrderTotal field for a specific year and month
+    @Query("SELECT f.purchaseOrderTotal FROM FinanceStats f WHERE f.year = :year AND f.month = :month")
+    Optional<Double> findPurchaseOrderTotalByYearAndMonth(
+            @Param("year") int year,
+            @Param("month") int month
+    );
+
+    // This fetches ONLY the totalUnitsSold field for a specific year and month
+    @Query("SELECT f.totalUnitsSold FROM FinanceStats f WHERE f.year = :year AND f.month = :month")
+    Optional<Long> findTotalUnitsSoldByYearAndMonth(
+            @Param("year") int year,
+            @Param("month") int month
+    );
 }
