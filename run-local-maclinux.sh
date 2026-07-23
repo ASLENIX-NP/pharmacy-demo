2a6d09f574367f4ed7646ed894c71f636f975312
aayush rana magar <realnaramagaraayush@gmail.com>
2026-07-23 11:48:12 +0545

    update run-local-maclinux.sh to use environment variable placeholders for sensitive information

--- run-local-maclinux.sh
+++ run-local-maclinux.sh
@@ -1,10 +1,10 @@
 export DB_PORT="3306"
-export DB_NAME="pharmadbdemo"
-export DB_USERNAME="root"
-export DB_PASSWORD="Sudes@123##"
+export DB_NAME="DB_NAME"
+export DB_USERNAME="DB_USERNAME"
+export DB_PASSWORD="DB_PASSWORD"
 export DOMAIN="http://localhost:8080"
-export MAIL_PASSWORD="xoez arkj htld ircu"
-export MAIL_USERNAME="edutaskcalendar@gmail.com"
+export MAIL_PASSWORD="MAIL_PASSWORD"
+export MAIL_USERNAME="MAIL_USERNAME"
 
 ./mvnw spring-boot:run
 
