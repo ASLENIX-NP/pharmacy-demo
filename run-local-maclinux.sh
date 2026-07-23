12fa65e9f4eef89bfd9389f5aba5a2638b88a49c
Sonica-git <Sonica.Adhikari_22@eemc.edu.np>
2026-07-23 11:42:26 +0545

    run local for maclinux

---
+++ run-local-maclinux.sh
@@ -0,0 +1,10 @@
+export DB_PORT="3306"
+export DB_NAME="pharmadbdemo"
+export DB_USERNAME="root"
+export DB_PASSWORD="Sudes@123##"
+export DOMAIN="http://localhost:8080"
+export MAIL_PASSWORD="xoez arkj htld ircu"
+export MAIL_USERNAME="edutaskcalendar@gmail.com"
+
+./mvnw spring-boot:run
+
